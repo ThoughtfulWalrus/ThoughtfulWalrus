f8465804d81ceeeb2be770e131ecc9b4cc4dec91
Kyle Owen <benignbemine@gmail.com>
2015-03-16 16:33:08 -0700

    added comments

--- client/app/home/HomeCtrl.js
+++ client/app/home/HomeCtrl.js
@@ -1,16 +1,29 @@
 angular.module('distress')
 .controller('HomeCtrl', ['$scope', function($scope){
-  $scope.number = 'placeholder for emergency number';
+
+  $scope.emergencyNumber = 'placeholder for emergency number';
+
   $scope.locationData = 'placeholder for location';
+
+  //function which uses the dataFetcher object to grab the emergency number
+  //assumes that getLocation has already been run.
+  //passes location and callback to dataFetcher method, sets emergencyNumber on the DOM
   $scope.getEmergencyNumber = function(){
+    var self = this;
+    var location = {longitude: utils.longitude, latitude: utils.latitude};
+    dataFetcher.getEmergencyNumber(location, function(emergencyNumber){
+      self.emergencyNumber = emergencyNumber;
+    }.bind(self));
   };
-  $scope.setEmergencyNumber = function(){
-  };
+
+  //function which uses the utils object to grab the geolocation
+  //passes lat lon and callback, sets locationData on the DOM
   $scope.getLocation = function(){
     var self = this;
     utils.getLocation(function(lat, lon){
       this.locationData = 'latitude: ' + lat + ' longitude: ' + lon;
     }.bind(self));
-  }
+  };
+
 }]);
 
