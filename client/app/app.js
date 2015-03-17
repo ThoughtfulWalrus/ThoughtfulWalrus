9e0de7e3d3dbf4d182445e3b1a44341b73b4f6e2
Ben Rodigas <benjamin.rodigas@gmail.com>
2015-03-17 14:37:40 -0700

    add ui-router and Sass files

--- client/app/app.js
+++ client/app/app.js
@@ -1,23 +1,19 @@
-var app = angular.module('distress', ['ngRoute']);
+var app = angular.module('distress', ['ui.router']);
 
-app.config(function($routeProvider, $httpProvider){
-  $routeProvider
-    .when('/signup', {
-      templateUrl: '/signup/signup.html',
-      controller: 'SignupController',
-      authenticate: false
-    })
-    .when('/', {
-      templateUrl: 'app/home/home.html',
-      controller: 'HomeCtrl',
-      authenticate: false
-    })
-    .otherwise({
-      templateUrl: 'app/home/home.html',
-      controller: 'HomeCtrl',
-      authenticate: false
-    });
-})
-
+app.config(function($stateProvider, $urlRouterProvider) {    
+    $urlRouterProvider.otherwise('/');    
+    $stateProvider        
+      .state('home', {
+          url: '/',
+          controller: 'HomeCtrl',
+          templateUrl: 'app/home/home.html'
+      })
+      .state('signup', {
+          url: '/signup',
+          templateUrl: '/signup/signup.html',
+          controller: 'SignupController',
+          authenticate: false
+      });              
+});
 
 
