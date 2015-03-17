30721bc954b0a6445673a8130833931ce268697a
i-made-that <rogerthomasson@gmail.com>
2015-03-17 14:01:51 -0700

    Store map link in addition to lat/lon in utils.storeLocation

--- client/lib/utils.js
+++ client/lib/utils.js
@@ -20,11 +20,12 @@ var utils = (function() {
         },
 
         /// Function: storeLocation()
-        /// Description: Stores longitude and latitude to the window. takes callback
+        /// Description: Stores longitude, latitude, and google maps link to the window. takes callback
         /// returns: Nothing
         storeLocation: function(cb, position) {
           this.latitude = position.coords.latitude;
           this.longitude = position.coords.longitude;
+          this.mapLink = 'http://maps.google.com/?q=' + this.latitude + ',' + this.longitude;
           cb(this.latitude, this.longitude);
         }
     };
