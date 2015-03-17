fb7aa871867137d0c3ef4f6355ddc31f11be474e
Kyle Owen <benignbemine@gmail.com>
2015-03-17 10:37:16 -0700

    updating

--- client/lib/dataFetcher.js
+++ client/lib/dataFetcher.js
@@ -68,7 +68,7 @@ dataFetcher.getEmergencyNumber = function(coords, callback){
   var data = {};
 
   var loc = new google.maps.LatLng(coords.latitude, coords.longitude);
-
+  console.log('heyyyyy: ' + loc)
   var request = {
     location: loc,
     radius: '1000',
@@ -91,6 +91,7 @@ dataFetcher.getEmergencyNumber = function(coords, callback){
           data.country = results.formatted_address.split(',').slice(-1)[0].trim();
           data.emergencyNumber = nums[data.country];
           data.policeNumber = results.formatted_phone_number;
+          console.log(data.emergencyNumber);
           callback(data.emergencyNumber);
         }
       });
