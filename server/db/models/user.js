5a43b2535b09c2f10fe4bce862c9983412b42180
Ben Rodigas <benjamin.rodigas@gmail.com>
2015-03-16 18:46:26 -0700

    added comments to user.js

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,14 +1,17 @@
 var mongoose = require('mongoose');
+
+/// Create userSchema with Mongoose. The schema contains a username and an array of emergency contacts. 
 var userSchema = mongoose.Schema({
   username: { 
     type: String, 
     index: { unique: true },
   }, 
-  emergencyContacts:[{
+  emergencyContacts:[{ 
       contactName: String, 
       contactNumber: String
   }],
 });
+
 
 var User = mongoose.model('User', userSchema);
 
