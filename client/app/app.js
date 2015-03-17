ee87a6e07d74c4059c2659936dabf7b01d91f2ad
Kyle Owen <benignbemine@gmail.com>
2015-03-17 14:38:38 -0700

    updated app

--- client/app/app.js
+++ client/app/app.js
@@ -2,6 +2,7 @@ var app = angular.module('distress', ['ngRoute']);
 
 app.config(function($routeProvider, $httpProvider){
   $routeProvider
+    //note: instead of hardcoding the controller into the view, specifying the controller below will be enough!
     .when('/signup', {
       templateUrl: '/signup/signup.html',
       controller: 'SignupController',
