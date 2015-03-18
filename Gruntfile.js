bd0ffabdfaa757b33b68b4b46d40f7d1522ae1b7
i-made-that <rogerthomasson@gmail.com>
2015-03-18 14:40:27 -0700

    Include grunt-shell and grunt-config-cssmin in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,14 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-mocha');
   grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-shell');
 
   // in what order should the files be concatenated
   var clientIncludeOrder = require('./include.conf.js');
