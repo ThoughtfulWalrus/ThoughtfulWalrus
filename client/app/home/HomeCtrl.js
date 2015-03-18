df11cc5b12cb58a8ae96db815ea8946cf5611e2c
Kyle Owen <benignbemine@gmail.com>
2015-03-18 12:21:42 -0700

    can see police map on click

--- client/app/home/HomeCtrl.js
+++ client/app/home/HomeCtrl.js
@@ -1,5 +1,5 @@
 angular.module('distress')
-.controller('HomeCtrl', ['$scope', '$http', 'DistressButton', function($scope, $http, DistressButton){
+.controller('HomeCtrl', ['$scope', '$http', 'DistressButton', 'DataFetcher', 'GeoLocation', function($scope, $http, DistressButton, DataFetcher, GeoLocation){
 
   $scope.emergencyNumber = '';
 
@@ -9,25 +9,25 @@ angular.module('distress')
   $scope.username = 'anon';
 
   //assumes that getLocation has already been run.
-  //passes location and callback to dataFetcher method, sets emergencyNumber on the DOM
+  //passes location and callback to DataFetcher method, sets emergencyNumber on the DOM
   //using $apply (since it's asynchronous)
   $scope.getEmergencyNumber = function(){
     var self = this;
-    var location = {longitude: utils.longitude, latitude: utils.latitude};
+    var location = {longitude: GeoLocation.longitude, latitude: GeoLocation.latitude};
 
-    dataFetcher.getEmergencyNumber(location, function(emergencyNumber){
+    DataFetcher.getEmergencyNumber(location, function(emergencyNumber){
       this.$apply(function(){
         this.emergencyNumber = emergencyNumber
       }.bind(this));
     }.bind(self));
   };
 
-  //function which uses the utils object to grab the geolocation
+  //function which uses the GeoLocation object to grab the geolocation
   //passes lat lon and callback, sets locationData on the DOM using $apply (since it's asynchronous)
   //calls getEmergencyNumber after the location has been found
   $scope.getLocation = function(){
     var self = this;
-    utils.getLocation(function(lat, lon){
+    GeoLocation.getLocation(function(lat, lon){
       this.$apply(function(){
         this.locationData = 'latitude: ' + lat + ' longitude: ' + lon;
       }.bind(this));
@@ -39,6 +39,12 @@ angular.module('distress')
   //sends distress signal when the button is clicked
   $scope.distress = function(){
     DistressButton.sendDistress($scope.username);
+  }
+
+  //gets police station map
+  $scope.police = function(){
+    var location = {longitude: GeoLocation.longitude, latitude: GeoLocation.latitude};
+    DataFetcher.getPoliceMap(location);
   }
 
   // initializes location and emergency number
