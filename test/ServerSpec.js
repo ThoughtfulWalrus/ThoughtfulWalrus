e0eaf16f75484fdf400651dc2eb14b9812c8876b
puopg <michelchen101@gmail.com>
2015-03-18 12:13:24 -0700

    Updated server test cases and added comments

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -4,6 +4,9 @@ var request = require('request');
 var db = require('../server/db/config');
 var User = require('../server/db/models/user');
 
+/// User Actions - 1001 
+/// These test cases test User actions. These are things a user can perform such as
+/// signing in, signin out, or adding a contact. 
 describe('User Actions:', function() {
 
   var requestWithSession = request.defaults({jar: true});
@@ -23,7 +26,11 @@ describe('User Actions:', function() {
     done();
   });
 
+  /// User Sign-in Test cases:
   describe('User Signin:', function(){
+    /// Test Case: Signin-1001-001
+    /// This test case will test that the server responds with a 400 code 
+    /// when trying to sign in a user that does not currently exist
     it('Server should respond with 400 if user does not exist', function(done) {
         var options = {
           'method': 'POST',
@@ -41,6 +48,10 @@ describe('User Actions:', function() {
       });
     });
 
+    /// Test Case: Signin-1001-002
+    /// This test case will test that the server responds with a 200 code 
+    /// when trying to sign in a user that does exist. The server
+    /// should also send back the user object to the client. 
     it('Server should respond with 200 if user does exist and send back the user object', function(done) {
         var options = {
           'method': 'POST',
@@ -61,7 +72,11 @@ describe('User Actions:', function() {
     });
   }); // 'User Signin'
 
+//User Sign-up Test Cases:
 describe('User Signup:', function(){
+  /// Test Case: Signup-1001-001
+  /// This test case will test that the server responds with a 409 code 
+  /// when trying to signup a user that already has been added to the database.
   it('Server should respond with 409 if user already exists', function(done) {
       var options = {
         'method': 'POST',
@@ -80,6 +95,9 @@ describe('User Signup:', function(){
     });
   });
 
+  /// Test Case: Signup-1001-002
+  /// This test case will test that the server responds with a 200 code 
+  /// when trying to signup a new user.
   it('Server should respond with 200 if user was successfully added.', function(done) {
       var options = {
         'method': 'POST',
@@ -98,25 +116,13 @@ describe('User Signup:', function(){
       done();
     });
   });
-  it('Server should respond with 409 if user already exists.', function(done) {
-      var options = {
-        'method': 'POST',
-        'followAllRedirects': true,
-        'uri': 'http://127.0.0.1:5000/user/signup',
-        'json': {
-          'username': 'Fabio',
-          'password': 'password'
-        }
-      };
-
-    requestWithSession(options, function(error, res, body) {
-      expect(res.statusCode).to.equal(409);
-      done();
-    });
-  });
 }); // 'User Signup'
 
+/// User add contact Test cases:
 describe('User addContact:', function(){
+  /// Test Case: AddContact-1001-001
+  /// This test case will test that the server responds with a 401 code 
+  /// when trying to add a contact when the user is not logged in / authorized.
   it('Server should respond with 401 if user is not authorized', function(done) {
       var options = {
         'method': 'POST',
@@ -136,6 +142,9 @@ describe('User addContact:', function(){
     });
   });
 
+  /// Test Case: AddContact-1001-002
+  /// This test case will test that the server responds with a 200 code 
+  /// when trying to add a contact that does not exist for that given user. 
   it('Server should respond with 200 when adding a new contact.', function(done) {
       var options = {
         'method': 'POST',
@@ -158,6 +167,9 @@ describe('User addContact:', function(){
     });
   });
 
+  /// Test Case: AddContact-1001-003
+  /// This test case will test that the server responds with a 409 code 
+  /// when trying to add a contact number which already exists in the users contact list. 
   it('Server should respond with 409 if contact to add already exists', function(done) {
       var options = {
         'method': 'POST',
@@ -170,18 +182,24 @@ describe('User addContact:', function(){
         }
       };
 
-    requestWithSession(options, function(error, res, body) {
-      expect(res.statusCode).to.equal(200);
-      requestWithSession(options, function(error, res, body) {
-        expect(res.statusCode).to.equal(409);
-        done();
-      });
-    });
+      User.findOne({ username: 'Fabio' })
+        .exec(function(err, user) {
+          user.emergencyContacts.push({contactName:'ContactName', contactNumber: '1112223333'});
+          user.save(function(err, user){
+            requestWithSession(options, function(error, res, body) {
+              expect(res.statusCode).to.equal(409);
+              done();
+            });
+          });
+        });
   });
 }); // 'User addContact'
 
 }); // 'User Actions'
 
+/// SMS Text Actions - 2001
+/// These test cases test SMS controller actions. These are things the SMS module is responsible
+/// for handling, mainly that being texting people. 
 describe('SMS Actions:', function() {
 
     var requestWithSession = request.defaults({jar: true});
@@ -201,6 +219,9 @@ describe('SMS Actions:', function() {
       done();
     });
 
+    /// Test Case: SMSText-2001-001
+    /// This test case will test that the server responds with a 401 code 
+    /// when trying to text without being logged in / authorized.
     describe('SMS Send:', function(){
       it('Server should respond with 401 if user is not logged-in/authorized', function(done) {
           var options = {
@@ -220,6 +241,10 @@ describe('SMS Actions:', function() {
         });
       });
 
+      /// Test Case: SMSText-2001-002
+      /// This test case will test that the server responds with a 200 code 
+      /// when trying to text an empty list of contacts. An empty message status
+      /// array should be returned.
       it('Server should respond with 200 and empty array when there are no contacts', function(done) {
           var options = {
             'method': 'POST',
@@ -238,93 +263,87 @@ describe('SMS Actions:', function() {
         });
       });
 
+      /// Test Case: SMSText-2001-003
+      /// This test case will test that the server responds with a 200 code 
+      /// when trying to text a list of contacts. An array of status objects is returned for each
+      /// text corresponding to the success/failure of each. 
       it('Server should respond with 200 and response array when there are contacts', function(done) {
-        this.timeout(10000)
+        var contactList = [{contactName:'Mom', contactNumber: '1111111111'},
+                           {contactName:'Dad', contactNumber: '2222222222'},
+                           {contactName:'Sister', contactNumber: '3333333333'}];
         var options = {
                 'method': 'POST',
                 'followAllRedirects': true,
-                'uri': 'http://127.0.0.1:5000/user/addContact',
+                'uri': 'http://127.0.0.1:5000/sms/text',
                 'json': {
-                  'username': 'Fabio',
-                  'contactName': 'ContactName',
-                  'contactNumber': '1112223333'
-                }
-              };
+                  'username': 'Fabio'
+                 }
+        };
 
-            requestWithSession(options, function(error, res, body) {
-              expect(res.statusCode).to.equal(200);
+        User.findOne({ username: 'Fabio' })
+          .exec(function(err, user) {
+            contactList.forEach(function(number){
+              user.emergencyContacts.push(number);
+            });
+            user.save(function(err, user){
+              requestWithSession(options, function(error, res, body) {
+                expect(res.statusCode).to.equal(200);
+                expect(res.body[0].message).to.contain('1111111111');
+                expect(res.body[1].message).to.contain('2222222222');
+                expect(res.body[2].message).to.contain('3333333333');
+                done();
+              });
+            });
+          });
+      });
 
-              var options2 = {
+    /// Test Case: SMSText-2001-004
+    /// This test case will test that the server responds with a 200 code 
+    /// when trying to text a list of contacts. An array of status objects is returned for each
+    /// text corresponding to the success/failure of each. This array should come back in 
+    /// the same order of the texts sent. 
+    it('Server should respond with SMS status in correct order', function(done) {
+      var contactList = [{contactName:'Mom', contactNumber: '1111111111'},
+                         {contactName:'Dad', contactNumber: '2222222222'},
+                         {contactName:'Sister', contactNumber: '3333333333'},
+                         {contactName:'Brother', contactNumber: '4444444444'},
+                         {contactName:'Aunt', contactNumber: '5555555555'},
+                         {contactName:'Uncle', contactNumber: '6666666666'},
+                         {contactName:'Grandmother', contactNumber: '7777777777'},
+                         {contactName:'Best Friend', contactNumber: '8888888888'},
+                         {contactName:'My cat', contactNumber: '9999999999'},
+                         {contactName:'God', contactNumber: '0000000000'}];
+              var options = {
                       'method': 'POST',
                       'followAllRedirects': true,
                       'uri': 'http://127.0.0.1:5000/sms/text',
                       'json': {
                         'username': 'Fabio'
-                      }
-                    };
-              requestWithSession(options2, function(error, res, body) {
-                expect(res.statusCode).to.equal(200);
-                done();
-              });
-            });
-        });
+                       }
+              };
 
-    it('Server should respond with SMS status in correct order', function(done) {
-      this.timeout(10000)
-      var options1 = {
-              'method': 'POST',
-              'followAllRedirects': true,
-              'uri': 'http://127.0.0.1:5000/user/addContact',
-              'json': {
-                'username': 'Fabio',
-                'contactName': 'ContactName',
-                'contactNumber': '1111111111'
-              }
-            };
-      var options2 = {
-              'method': 'POST',
-              'followAllRedirects': true,
-              'uri': 'http://127.0.0.1:5000/user/addContact',
-              'json': {
-                'username': 'Fabio',
-                'contactName': 'ContactName2',
-                'contactNumber': '2222222222'
-              }
-            };
-      var options3 = {
-              'method': 'POST',
-              'followAllRedirects': true,
-              'uri': 'http://127.0.0.1:5000/user/addContact',
-              'json': {
-                'username': 'Fabio',
-                'contactName': 'ContactName',
-                'contactNumber': '3333333333'
-              }
-            };        
-          requestWithSession(options1, function(error, res, body) {
-            expect(res.statusCode).to.equal(200);
-          });
-          requestWithSession(options2, function(error, res, body) {
-            expect(res.statusCode).to.equal(200);
-          });
-          requestWithSession(options3, function(error, res, body) {
-            expect(res.statusCode).to.equal(200);
-            var textOption = {
-                    'method': 'POST',
-                    'followAllRedirects': true,
-                    'uri': 'http://127.0.0.1:5000/sms/text',
-                    'json': {
-                      'username': 'Fabio'
-                    }
-                  };
-            requestWithSession(textOption, function(error, res, body) {
-              expect(res.statusCode).to.equal(200);
-              expect(res.body[0].message).to.contain('1111111111');
-              expect(res.body[1].message).to.contain('2222222222');
-              expect(res.body[2].message).to.contain('3333333333');
-              done();
-            });
-          });
+              User.findOne({ username: 'Fabio' })
+                .exec(function(err, user) {
+                  contactList.forEach(function(number){
+                    user.emergencyContacts.push(number);
+                  });
+                  user.save(function(err, user){
+                    requestWithSession(options, function(error, res, body) {
+                      expect(res.statusCode).to.equal(200);
+                      expect(res.body[0].message).to.contain('1111111111');
+                      expect(res.body[1].message).to.contain('2222222222');
+                      expect(res.body[2].message).to.contain('3333333333');
+                      expect(res.body[3].message).to.contain('4444444444');
+                      expect(res.body[4].message).to.contain('5555555555');
+                      expect(res.body[5].message).to.contain('6666666666');
+                      expect(res.body[6].message).to.contain('7777777777');
+                      expect(res.body[7].message).to.contain('8888888888');
+                      expect(res.body[8].message).to.contain('9999999999');
+                      expect(res.body[9].message).to.contain('0000000000');
+                      done();
+                    });
+                  });
+                });
           
       });
     }); // 'SMS Send'
