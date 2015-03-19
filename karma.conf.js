9a91f21c6cd28c85eb7d7f2ae843cd5a7f26172f
Kyle Owen <benignbemine@gmail.com>
2015-03-18 22:33:05 -0700

    check methods exist

--- karma.conf.js
+++ karma.conf.js
@@ -7,8 +7,10 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: require('./include.conf.js').concat([
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
       'test/unit/**/*.js',
-      'test/integration/**/*.js'
+      'test/integration/**/*.js',
     ]),
 
     // list of files to exclude
