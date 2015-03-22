b33308eb15e74ca49939fe6e241b45ec8e7a8080
Kyle Owen <benignbemine@gmail.com>
2015-03-22 16:58:40 -0700

    added iife

--- client/app/hospitalMap/HospitalCtrl.js
+++ client/app/hospitalMap/HospitalCtrl.js
@@ -1,15 +1,22 @@
-angular.module('distress')
-  .controller('HospitalCtrl', ['$scope', 'DataFetcher', 'GeoLocation',function ($scope, DataFetcher, GeoLocation) {
+(function(){
+
+  angular
+    .module('distress')
+    .controller('HospitalCtrl', HospitalCtrl);
+
+  HospitalCtrl.$inject = ['$scope', 'DataFetcher', 'GeoLocation'];
+
+  function HospitalCtrl($scope, DataFetcher, GeoLocation){
     $scope.init = function(){
       $scope.hospital();
-    }
+    };
 
     //gets hospital station map
     $scope.hospital = function(){
       var location = {longitude: GeoLocation.longitude, latitude: GeoLocation.latitude};
       DataFetcher.getHospitalMap(location);
-    }
+    };
 
     $scope.init();
-
-  }]);
+  }
+})();
