35c849bfa55708ce3b92d79400b49ee96343ee18
Kyle Owen <benignbemine@gmail.com>
2015-03-22 16:21:49 -0700

    cleaned up and added iife

--- client/app/home/HomeCtrl.js
+++ client/app/home/HomeCtrl.js
@@ -1,69 +1,66 @@
-HomeCtrl.$inject = ['$scope', '$http', 'DistressButton', 'DataFetcher', 'GeoLocation', 'Auth']
-angular.module('distress').controller('HomeCtrl', HomeCtrl);
+(function(){
 
-function HomeCtrl($scope, $http, DistressButton, DataFetcher, GeoLocation, Auth){
-  $scope.emergencyNumber = DataFetcher.savedNumber || '';
-  $scope.locationData = {};
-  $scope.isLoggedIn = Auth.isAuthenticated();
-  console.log($scope.isLoggedIn)
+  angular
+    .module('distress')
+    .controller('HomeCtrl', HomeCtrl);
 
-  $scope.spinner = true;
-  $scope.homeContent   = false;
+  HomeCtrl.$inject = ['$scope', '$http', 'DistressButton', 'DataFetcher', 'GeoLocation', 'Auth']
 
-  //assumes that getLocation has already been run.
-  //passes location and callback to DataFetcher method, sets emergencyNumber on the DOM
-  //using $apply (since it's asynchronous)
-  //store it to Datafetcher.savedNumber so that it persists.
-  
-  $scope.spinner = true;
-  $scope.homeContent   = false;
+  function HomeCtrl($scope, $http, DistressButton, DataFetcher, GeoLocation, Auth){
+    $scope.emergencyNumber = DataFetcher.savedNumber || '';
+    $scope.locationData = {};
+    $scope.isLoggedIn = Auth.isAuthenticated();
+    $scope.spinner = true;
+    $scope.homeContent   = false;
 
+    //assumes that getLocation has already been run.
+    //passes location and callback to DataFetcher method, sets emergencyNumber on the DOM
+    //using $apply (since it's asynchronous)
+    //store it to Datafetcher.savedNumber so that it persists.
+    $scope.getEmergencyNumber = function(){
+      var self = this,
+          location = {longitude: GeoLocation.longitude, latitude: GeoLocation.latitude};
 
-  $scope.getEmergencyNumber = function(){
-    var self = this;
-    var location = {longitude: GeoLocation.longitude, latitude: GeoLocation.latitude};
-
-    DataFetcher.getEmergencyNumber(location, function(emergencyNumber){
-      self.$apply(function(){
-        self.emergencyNumber = emergencyNumber;
-        DataFetcher.savedNumber = emergencyNumber;
+      DataFetcher.getEmergencyNumber(location, function(emergencyNumber){
+        self.$apply(function(){
+          self.emergencyNumber = emergencyNumber;
+          DataFetcher.savedNumber = emergencyNumber;
+          self.spinner = false;
+          self.homeContent = true;
+        });
       });
-    });
+    };
 
-    $scope.spinner = false;
-    $scope.homeContent = true;
+    //function which uses the GeoLocation object to grab the geolocation
+    //passes lat lon and callback, sets locationData on the DOM using $apply (since it's asynchronous)
+    //calls getEmergencyNumber after the location has been found
+    $scope.getLocation = function(){
+      var self = this;
 
-  };
-
-  //function which uses the GeoLocation object to grab the geolocation
-  //passes lat lon and callback, sets locationData on the DOM using $apply (since it's asynchronous)
-  //calls getEmergencyNumber after the location has been found
-  $scope.getLocation = function(){
-    var self = this;
-
-    GeoLocation.getLocation(function(lat, lon){
-      self.$apply(function(){
-        self.locationData = {latitude: lat, longitude: lon};
+      GeoLocation.getLocation(function(lat, lon){
+        self.$apply(function(){
+          self.locationData = {latitude: lat, longitude: lon};
+        });
+        //once we get location data, we get emergency number
+        self.getEmergencyNumber();
       });
-      //once we get location data, we get emergency number
-      self.getEmergencyNumber();
-    });
-  };
+    };
 
-  //sends distress signal when the button is clicked
-  $scope.distress = function(){
-    DistressButton.sendDistress();
-  };
+    //sends distress signal when the button is clicked
+    $scope.distress = function(){
+      DistressButton.sendDistress();
+    };
 
-  //gets police station map
-  $scope.police = function(){
-    var location = {longitude: GeoLocation.longitude, latitude: GeoLocation.latitude};
-    DataFetcher.getPoliceMap(location);
-  };
+    //gets police station map
+    $scope.police = function(){
+      var location = {longitude: GeoLocation.longitude, latitude: GeoLocation.latitude};
+      DataFetcher.getPoliceMap(location);
+    };
 
-  // initializes location and emergency number
-  $scope.init = function(){
-    $scope.getLocation();
-  };
-  $scope.init();
-}
+    // initializes location and emergency number
+    $scope.init = function(){
+      $scope.getLocation();
+    };
+    $scope.init();
+  }
+})();
