aef8aa7a14a1940af1e38835dedda19449b95d69
Kyle Owen <benignbemine@gmail.com>
2015-03-21 20:21:13 -0700

    duplicate users

--- client/app/signup/signupCtrl.js
+++ client/app/signup/signupCtrl.js
@@ -1,14 +1,17 @@
 angular.module('distress')
 .controller('SignupCtrl', ['$scope', '$window', 'Auth', '$location', function($scope, $window, Auth, $location){
   $scope.user = {};
+  $scope.inputError = false;
+
   $scope.signUp = function(){
     Auth.signUp($scope.user)
       .then(function (response) {
         $window.localStorage.setItem('distressAuth', response.data.token);
         $location.path('/');
+        $scope.inputError = false;
       })
       .catch(function (error) {
-        console.error(error);
+        $scope.inputError = true;
       });
   };
 }]);
