63aa11e6470a2da5da52471db23b03383bdb86ef
Kyle Owen <benignbemine@gmail.com>
2015-03-22 17:23:39 -0700

    added iife

--- client/app/signin/signinCtrl.js
+++ client/app/signin/signinCtrl.js
@@ -1,15 +1,25 @@
-angular.module('distress')
-.controller('SigninCtrl', ['$scope', '$window', 'Auth', '$location', function($scope, $window, Auth, $location){
-  $scope.user = {};
-  $scope.signIn = function(){
-    Auth.signIn($scope.user)
-      .then(function (response) {
-        $window.localStorage.setItem('distressAuth', response.data.token);
-        console.log('yayyy');
-        $location.path('/');
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  };
-}]);
+(function(){
+
+  angular
+    .module('distress')
+    .controller('SigninCtrl', SigninCtrl);
+
+
+  SigninCtrl.$inject = ['$scope', '$window', 'Auth', '$location'];
+
+
+  function SigninCtrl($scope, $window, Auth, $location){
+    $scope.user = {};
+    $scope.signIn = function(){
+      Auth.signIn($scope.user)
+        .then(function (response) {
+          $window.localStorage.setItem('distressAuth', response.data.token);
+          console.log('yayyy');
+          $location.path('/');
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    };
+  }
+})();
