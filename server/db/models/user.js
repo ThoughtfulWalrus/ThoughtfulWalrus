137da4297c676f764056c5a8c5d6564a2d25add0
puopg <michelchen101@gmail.com>
2015-03-23 16:05:17 -0700

    Beginning changes for real username.

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -7,6 +7,8 @@ var SALT_WORK_FACTOR = 10;
 var userSchema = mongoose.Schema({
   username: {type: String, index: { unique: true }, require: true},
   password:{type: String, require: true},
+  firstName: {type: String, require: true},
+  lastName: {type: String, require: true},
   emergencyContacts:[{name: String, phone: String, lastMsgStatus: String}],
   salt: String
 });
