30de1fba2e4123f5957d0e04d8b2a4dcb7c8b107
puopg <michelchen101@gmail.com>
2015-03-23 09:46:54 -0700

    Adding more detailed message to distress text. Need to add type still.

--- client/app/distressButton/distressButton.js
+++ client/app/distressButton/distressButton.js
@@ -19,10 +19,14 @@
     //sends username to server, which will grab
     //the contactList from the database and send the
     //messages.
-    function sendDistress(){
+    function sendDistress(latitude, longitude, mapLink, dateTime){
       $http({
         method: 'POST',
         url: '/sms/text/',
+        data: {latitude: latitude, 
+               longitude: longitude, 
+               mapLink: mapLink, 
+               timeOfDistress: dateTime}
       }).then(function(response){
         return response;
       });
