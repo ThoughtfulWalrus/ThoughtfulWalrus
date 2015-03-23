d0d03913ee98e4377dacee887d34499d37c54c41
Kyle Owen <benignbemine@gmail.com>
2015-03-22 17:05:29 -0700

    cleanup

--- client/app/navbar/navbarCtrl.js
+++ client/app/navbar/navbarCtrl.js
@@ -1,6 +1,14 @@
-angular.module('distress')
-.controller('NavbarCtrl', ['$scope', 'Auth', function ($scope, Auth) {
-  $scope.showContacts = function(){
-    return Auth.isAuthenticated();
+(function(){
+
+  angular
+    .module('distress')
+    .controller('NavbarCtrl', NavbarCtrl);
+
+  NavbarCtrl.$inject = ['$scope', 'Auth'];
+
+  function NavbarCtrl($scope, Auth){
+    $scope.showContacts = function(){
+      return Auth.isAuthenticated();
+    };
   }
-}]);
+})();
